99276fcd692e31baafcbb36fd399702bb44cba3a
ashrafshj <66910057+ashrafshj@users.noreply.github.com>
2026-09-01 23:51:28 +0400

    Implement m3u8 link retrieval from YouTube URL
    Added functionality to retrieve m3u8 link from YouTube streams.

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-$ytUrl = $_GET['url'] ?? 'https://youtu.be/4Ng7A95vHT0';
-header("Location: " . $ytUrl);
-exit();
+$youtube_url = isset($_GET['url']) ? $_GET['url'] : 'https://www.youtube.com/watch?v=4Ng7A95vHT0';
+
+// YouTube സ്ട്രീമുകളിൽ നിന്ന് m3u8 ലിങ്ക് എടുക്കാനുള്ള ലളിതമായ വഴി
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $youtube_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)');
+$html = curl_exec($ch);
+curl_close($ch);
+
+if (preg_match('/hlsManifestUrl\": \"(.*?)\"/', $html, $matches)) {
+    $m3u8_url = str_replace('\\/', '/', $matches[1]);
+    header("Location: " . $m3u8_url);
+    exit();
+} else {
+    http_response_code(404);
+    echo "Stream not found";
+}
 ?>
